898879ab90a9810e9b9bc57d25537e3a350786ae
Weder Videira Borges <wederborgesemp@gmail.com>
2026-07-01 20:36:47 -0400

    Ajustando .env do alembic

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+
+from .carrinho import Carrinho, CarrinhoItens
+from .empresas import Empresas, Filiais
+from .pedidos import Pedidos, PedidoItens, PedidoStatusHistorico
+from .produtos import Produtos
+from .usuarios import User
