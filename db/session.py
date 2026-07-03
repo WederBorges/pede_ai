3d02b4a5d0395b514acb71102e44e195d52fb10e
Weder Videira Borges <wederborgesemp@gmail.com>
2026-07-02 20:52:33 -0400

    Desenvolvendo conexão com banco Postgres

---
+++ db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+async_engine = create_async_engine(os.getenv("DATABASE_URL"))
+
+async def async_get_session():
+
+    async with AsyncSession(async_engine) as async_session:
+        yield async_session
+
