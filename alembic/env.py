79998cacb3a4e4313c9942621bce808d14661ca0
Wede _Borges <wederborgesemp@gmail.com>
2026-07-03 13:34:03 -0400

    configurando alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,12 +7,17 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from db.base import Base
 import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- db/session.py
+++ db/session.py
@@ -1,7 +1,9 @@
+import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
@@ -14,3 +16,8 @@ async def async_get_session():
     async with AsyncSession(async_engine) as async_session:
         yield async_session
 
+async def testar():
+    async with async_engine.connect() as conn:
+        print("conexão ok")
+
+asyncio.run(testar())
