fa3cd284259b91e73a3cf18607d63058356ff6cf
Weder Videira Borges <wederborgesemp@gmail.com>
2026-07-07 19:46:00 -0400

    Definindo e testando rota post

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from fastapi import FastAPI
 from fastapi import Depends
 from pydantic import BaseModel
 from datetime import datetime
+
+from starlette.status import HTTP_201_CREATED
 from models.empresas import Empresas
-from pede_ai.db.sessions import async_get_session
+from db.sessions import async_get_session
 from typing import Any
 
 app = FastAPI()
 
 
 class Empresas_update(BaseModel):
-    id: str
+    id: int
     nome: str
-    centro_de_custo: str
+    centro_de_custo: int
     created_at: datetime
     ativo: bool
-
 
 
 
@@ -30,9 +31,20 @@ def empresas():
         'empresas':['a','b','c','d', 'f', 'g']
     }
 
-@app.put('/')
+@app.post('/', status_code=201 ,response_model=Empresas_update)
 async def inputar_empresas(
     dados: Empresas_update,
-    session = Depends(async_get_session)
+    session = Depends(async_get_session),
+    
 ):
-   session.add()
+   empresa = Empresas(
+
+    nome = dados.nome,
+    centro_de_custo = dados.centro_de_custo,
+    ativo = dados.ativo
+   )
+   
+   session.add(empresa)
+   await session.commit()
+   await session.refresh(empresa)
+   return empresa
