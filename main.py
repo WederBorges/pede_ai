e9f63ca75444eaedb735f46256010806b3b91fcf
Weder Videira Borges <wederborgesemp@gmail.com>
2026-07-09 20:03:12 -0400

    desenvolvendo routers com APIRouter

--- main.py
+++ main.py
@@ -1,44 +1,8 @@
 from fastapi import FastAPI
-from fastapi import Depends
 
-from starlette.status import HTTP_201_CREATED
-from db.sessions import async_get_session
-from typing import Any
-
-from models.empresas import Empresas
-from schemas.schema_empresas import s_Empresas_out, s_Empresas_create, s_Empresas_response
-from sqlalchemy import select
+from routers import empresas
 
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"hello": "word"}
-
-@app.get('/empresas', status_code=200, response_model=s_Empresas_response)
-async def empresas(session = Depends(async_get_session)):
-    
-    empresas = await session.scalars(select(Empresas))
-
-    return  {
-        'empresas':empresas.all()
-    }
-
-@app.post('/', status_code=201 ,response_model=s_Empresas_out)
-async def inputar_empresas(
-    dados: s_Empresas_create,
-    session = Depends(async_get_session),
-    
-):
-   empresa = Empresas(
-
-    nome = dados.nome,
-    centro_de_custo = dados.centro_de_custo,
-    ativo = dados.ativo
-   )
-   
-   session.add(empresa)
-   await session.commit()
-   await session.refresh(empresa)
-   return empresa
+app.include_router(empresas.router)
---
+++ routers/empresas.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, status
+from fastapi import Depends
+
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
+from db.sessions import async_get_session
+from typing import Any
+
+from models.empresas import Empresas
+from schemas.schema_empresas import s_Empresas_out, s_Empresas_create, s_Empresas_response
+from sqlalchemy import select
+
+from fastapi import HTTPException
+from http import HTTPStatus
+
+router = APIRouter(prefix='/empresas')
+
+@router.get('/', status_code=200, response_model=s_Empresas_response)
+async def ler_empresas(session = Depends(async_get_session)):
+    
+    empresas = await session.scalars(select(Empresas))
+
+    return  {
+        'empresas':empresas.all()
+    }
+
+
+@router.get('/{id_user}' ,status_code=HTTPStatus.OK, response_model=s_Empresas_out)
+async def ler_empresa_unica(id_user: int, session=Depends(async_get_session)):
+    
+    stmt = select(Empresas).where(Empresas.id == id_user)
+    empresa = await session.scalar(stmt)
+
+    
+    if empresa:
+        return empresa
+
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail="Empresa não encontrada"
+    )
+
+@router.post('/', status_code=201 ,response_model=s_Empresas_out)
+async def inputar_empresas(
+    dados: s_Empresas_create,
+    session = Depends(async_get_session),
+    
+):
+   empresa = Empresas(
+
+    nome = dados.nome,
+    centro_de_custo = dados.centro_de_custo,
+    ativo = dados.ativo
+   )
+   
+   session.add(empresa)
+   await session.commit()
+   await session.refresh(empresa)
+   return empresa
